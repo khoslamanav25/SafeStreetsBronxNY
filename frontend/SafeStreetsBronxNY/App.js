a2d6a56bf29bb9c8e36c9c08795e0ff522f52069
khoslamanav25 <92478501+khoslamanav25@users.noreply.github.com>
2024-05-27 12:50:08 -0400

    App.js functionality

--- frontend/SafeStreetsBronxNY/App.js
+++ frontend/SafeStreetsBronxNY/App.js
@@ -2,9 +2,14 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { registerRootComponent } from 'expo';
 import React, { useEffect, useState } from 'react';
+import {  Text, Platform,  View } from 'react-native';
 import { onAuthStateChanged } from 'firebase/auth';
 import { FIREBASE_AUTH } from './FirebaseConfig';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Entypo } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
 
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
@@ -13,11 +18,105 @@ import HomeScreen from './screens/HomeScreen';
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
+const screenOptions = {
+  tabBarShowLabel:false,
+  headerShown:false,
+  tabBarStyle:{
+    position: "absolute",
+    bottom: 0,
+    right: 0,
+    left: 0,
+    elevation: 0,
+    height: 90,
+    background: "#fff"
+  }
+}
+
+
 function Tabs() {
   return (
-    <Tab.Navigator initialRouteName="Home">
-      <Tab.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
-    </Tab.Navigator>
+    <Tab.Navigator screenOptions={screenOptions}>
+          <Tab.Screen 
+          name="Home" 
+          component={HomeScreen} 
+          options={{
+            tabBarIcon: ({focused})=>{
+              return (
+                <View style={{alignItems: "center", justifyContent: "center"}}> 
+                  <Entypo name="home" size={24} color={focused ? "#16247d": "#111"} />
+                  <Text style={{fonSize: 12, color: "#16247d"}}>Home</Text>
+            </View>
+              )
+            }
+          }}
+          />
+          <Tab.Screen 
+          name="Map" 
+          component={HomeScreen} 
+          options={{
+            tabBarIcon: ({focused})=>{
+              return (
+                <View style={{alignItems: "center", justifyContent: "center"}}> 
+                 <MaterialIcons name="public" size={24} color={focused ? "#16247d": "#111"} />
+                  <Text style={{fonSize: 12, color: "#16247d"}}>Map</Text>
+            </View>
+              )
+            }
+          }}
+          />
+          <Tab.Screen 
+          name="Report" 
+          component={HomeScreen} 
+           options={{
+            tabBarIcon: ({focused})=>{
+              return (
+                <View
+                 style={{
+                  alignItems: "center",
+                  justifyContent: "center",
+                  backgroundColor: "#16247d",
+                  width: Platform.OS == "ios" ? 70 : 60,
+                  height: Platform.OS == "ios" ? 70 : 60,
+                  top: Platform.OS == "ios" ? -25 : -20,
+                  borderRadius: Platform.OS == "ios" ? 25 : 30
+                 }}
+                >
+                  <MaterialIcons name="report" size={40} color="#fff" />
+    
+                </View>
+              )
+            }
+           }}
+          />
+          <Tab.Screen
+           name="News" 
+           component={HomeScreen}
+           options={{
+            tabBarIcon: ({focused})=>{
+              return (
+                <View style={{alignItems: "center", justifyContent: "center"}}> 
+                 <MaterialIcons name="announcement" size={24} color={focused ? "#16247d": "#111"} />
+                  <Text style={{fonSize: 12, color: "#16247d"}}>News</Text>
+            </View>
+              )
+            }
+          }}
+           />
+          <Tab.Screen 
+          name="Settings" 
+          component={HomeScreen} 
+          options={{
+            tabBarIcon: ({focused})=>{
+              return (
+                <View style={{alignItems: "center", justifyContent: "center"}}> 
+                 <Ionicons name="settings" size={24}  color={focused ? "#16247d": "#111"} />
+                  <Text style={{fonSize: 12, color: "#16247d"}}>Settings</Text>
+            </View>
+              )
+            }
+          }}
+          />
+       </Tab.Navigator>
   );
 }
 
@@ -38,7 +137,9 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
         {user ? (
+  
           <Stack.Screen name="Tabs" component={Tabs} options={{ headerShown: false }} />
+        
         ) : (
           <>
             <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
